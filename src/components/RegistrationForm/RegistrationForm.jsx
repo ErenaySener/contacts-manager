dce2adb071219b8d2917a2f58a3e42ce6de98293
ErenaySener <erenaysener@hotmail.com>
2026-09-23 12:27:39 +0200

    Style registration form

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -13,23 +13,43 @@ export default function RegistrationForm() {
         actions.resetForm();
       }}
     >
-      <Form>
-        <label>
-          Name
-          <Field type="text" name="name" required />
+      <Form className="authForm">
+        <label className="formField">
+          <span>Name</span>
+          <Field
+            className="formInput"
+            type="text"
+            name="name"
+            placeholder="Your name"
+            required
+          />
         </label>
 
-        <label>
-          Email
-          <Field type="email" name="email" required />
+        <label className="formField">
+          <span>Email</span>
+          <Field
+            className="formInput"
+            type="email"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
         </label>
 
-        <label>
-          Password
-          <Field type="password" name="password" required />
+        <label className="formField">
+          <span>Password</span>
+          <Field
+            className="formInput"
+            type="password"
+            name="password"
+            placeholder="Create a password"
+            required
+          />
         </label>
 
-        <button type="submit">Register</button>
+        <button className="authButton" type="submit">
+          Create account
+        </button>
       </Form>
     </Formik>
   );
